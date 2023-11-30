f7c9749ea234ab62d3f6fe89e6865887a8246675
Sasha <tredipon@gmail.com>
2023-11-30 17:29:59 +0200

    Ми Інформацію про студентів

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 class Group_1012:
-    pass
-student1 = Group_1012()
-student1 = Group_1012()
-student1 = Group_1012()
-student1 = Group_1012()
-student1 = Group_1012()
+    def __init__(self, name, gen, age, evarage_mark):
+        self.name = name
+        self.gen = gen
+        self.age = age
+        self.evarage_mark = evarage_mark
+
+
+student1 = Group_1012("Гончарук Олександр", "чол", 12, 12)
+student1 = Group_1012("Трофимишин Владислав", "чол", 13,10)
+student1 = Group_1012("Коваленко Макар", "чол", 13, 10)
+student1 = Group_1012("Радомська Дарина",  "жін.",  13,  11)
+student1 = Group_1012("Юраш Максим", "чол", 14, 10)
 
